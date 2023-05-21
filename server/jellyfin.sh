05138e2a6ccf39a94de991e903a3d95f398e6f1c
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-05-21 07:25:54 -0400

    disable firewall

--- server/jellyfin.sh
+++ server/jellyfin.sh
@@ -2,3 +2,4 @@ sudo apt install curl -y
 curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
 sudo systemctl enable jellyfin.service --now
 sudo systemctl start jellyfin.service
+sudo ufw disable
