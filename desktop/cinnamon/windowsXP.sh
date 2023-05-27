da2f0db1491e040c5a87d8514aec2d5080edb89c
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-05-27 14:03:57 -0400

    load settings

--- desktop/cinnamon/windowsXP.sh
+++ desktop/cinnamon/windowsXP.sh
@@ -2,5 +2,7 @@ wget https://archive.org/download/bliss-600dpi/bliss-600dpi.png -P ~/Pictures
 git clone https://github.com/B00merang-Project/Windows-XP.git Windows-XP-Themes
 git clone https://github.com/B00merang-Artwork/Windows-XP.git Windows-XP-Icons
 mkdir -p ~/.themes ~/.icons
-mv -r 'Windows-XP-Themes/Windows XP Luna' ~/.themes
-mv -r 'Windows-XP-Icons/' ~/.icons
+cp -r 'Windows-XP-Themes/Windows XP Luna' ~/.themes
+cp -r 'Windows-XP-Icons/' ~/.icons
+sleep 3
+dconf load / < windowsXP.conf
